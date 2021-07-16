bcccbc391eacbcfa90dd21a41adbc62a8b353824
Dar Dualba <ddualba@gmail.com>
2021-07-15 21:24:58 -0700

    finish watchit project

---
+++ Proj_watchit/index.js
@@ -0,0 +1,40 @@
+#!/usr/bin/env node
+// mac/linux run cli  >> chmod +x index.js
+
+const debounce = require('lodash.debounce');
+const chokidar = require('chokidar');
+const program = require('caporal');
+const fs = require('fs');
+const { spawn } = require('child_process');
+const chalk = require('chalk');
+
+program
+  .version('1.0.0')
+  .argument('[filename]', 'Name of a file to execute')
+  .action(async ({ filename }) => {
+    const name = filename || 'index.js';
+
+    try {
+      await fs.promises.access(name);
+    } catch (err) {
+      throw new Error(`Could not find the file ${name}`);
+    }
+
+    let proc;
+    const start = debounce(() => {
+      if (proc) {
+        proc.kill();
+      }
+      console.log(chalk.blue('>>>> Starting process...'));
+      proc = spawn('node', [name], { stdio: 'inherit' });
+    }, 100);
+
+    chokidar
+      .watch('.')
+      .on('add', start)
+      .on('change', start)
+      .on('unlink', start);
+  });
+// .argument('<app>', 'App to deploy', /^myapp|their-app$/);
+
+program.parse(process.argv);
